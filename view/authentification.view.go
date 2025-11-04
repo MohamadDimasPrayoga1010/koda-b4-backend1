03733d5bf2c9e6154cc9b4c88b03b372cc194547
Mohamad Dimas Prayoga <mohamaddimasprayoga89@gmail.com>
2025-11-04 23:21:38 +0700

    feat: create a router for authentication

---
+++ view/authentification.view.go
@@ -0,0 +1,13 @@
+package view
+
+import (
+	"github.com/gin-gonic/gin"
+	"main.go/controller"
+)
+
+func UserAccount(r *gin.Engine){
+	var ua controller.Authentication
+
+	r.POST("/auth/register",  ua.Register) 
+	r.POST("auth/login", ua.Login)
+}
