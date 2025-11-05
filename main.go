3f0ab700b11aed614231249edcd41704116b3c49
Mohamad Dimas Prayoga <mohamaddimasprayoga89@gmail.com>
2025-11-06 01:55:23 +0700

    feat: added swagger module

--- main.go
+++ main.go
@@ -2,13 +2,22 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	"main.go/docs"
 	"main.go/view"
 )
 
-
-
+// @title API User & Auth Example Yoga
+// @version 1.0
+// @description Ini adalah dokumentasi API sederhana menggunakan Gin & Swagger
+// @termsOfService http://swagger.io/terms/
+// @host localhost:8085
+// @BasePath /
 func main() {
 	r := gin.Default()
 	view.InitView(r)
-	r.Run(":8080")
+	docs.SwaggerInfo.BasePath = "/"
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+	r.Run(":8085")
 }
